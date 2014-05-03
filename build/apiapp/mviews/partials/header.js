b333a8ae56811515013a0ce31a9bd719d1bca9c9
xinglie.lkf <xinglie.lkf@taobao.com>
2014-05-03 22:11:32 +0800

    调整下拉部分

--- build/apiapp/mviews/partials/header.js
+++ build/apiapp/mviews/partials/header.js
@@ -2,7 +2,7 @@
     author:xinglie.lkf@taobao.com
  */
 KISSY.add('apiapp/mviews/partials/header', function(S, View, Crox, MM, Magix) {
-     return View.extend({template:"<div class=\"toolbar clearfix\"> {{if data.isHome}} <h1>Magix API</h1> {{else}} <a class=icon-home href=\"#!/home\"><i class=iconfont>&#336;</i></a> {{if !data.isIndex}} <a class=icon-menu mx-click=\"toggleMenu<prevent>\" href=\"#!/home\"> <span class=icon-bar></span> <span class=icon-bar></span> <span class=icon-bar></span> </a> {{/if}} <div class=\"form-search clearfix\" style=\"margin-left:{{if data.isIndex}}50{{else}}85{{/if}}px\"> <input autocomplete=off value=\"{{if data.q}}{{data.q}}{{/if}}\" class=search type=text mx-keydown=search id=\"{{data.viewId}}_sipt\"  /> <i class=\"iconfont delete\">&#378;</i> <div class=btn-search mx-click=search><i class=iconfont name=search-btn>&#337;</i></div> </div> {{/if}} </div> {{if !data.isHome&&!data.isIndex}} <div class=menu-extended> <div class=extended-inner> <div class=clearfix> <div class=menu-outer> {{if data.extList&&data.extList.length}}<div class=menu-t>核心模块</div>{{/if}} <ul class=menu> {{each data.coreList as val}} <li class=menu-list> <a href=\"#!/{{data.infos.loader}}/{{data.infos.ver}}/{{val.name}}\">{{val.dName}}</a> </li> {{/each}} </ul> </div> {{if data.extList&&data.extList.length}} <div class=menu-outer> <div class=menu-t>扩展模块</div> <ul class=menu> {{each data.extList as val}} <li class=menu-list> <a href=\"#!/{{data.infos.loader}}/{{data.infos.ver}}/{{val.name}}\">{{val.dName}}</a> </li> {{/each}} </ul> </div> {{/if}} </div> </div> </div> {{/if}}",
+     return View.extend({template:"<div class=\"toolbar clearfix\"> {{if data.isHome}} <h1>Magix API</h1> {{else}} <a class=icon-home href=\"#!/home\"><i class=iconfont>&#336;</i></a> {{if !data.isIndex}} <a class=icon-menu mx-click=\"toggleMenu<prevent>({ver:{{data.infos.ver}}})\" href=\"#!/home\"> <span class=icon-bar></span> <span class=icon-bar></span> <span class=icon-bar></span> </a> {{/if}} <div class=\"form-search clearfix\" style=\"margin-left:{{if data.isIndex}}50{{else}}85{{/if}}px\"> <input autocomplete=off value=\"{{if data.q}}{{data.q}}{{/if}}\" class=search type=text mx-keydown=search id=\"{{data.viewId}}_sipt\"  /> <i class=\"iconfont delete\">&#378;</i> <div class=btn-search mx-click=search><i class=iconfont name=search-btn>&#337;</i></div> </div> {{/if}} </div> {{if !data.isHome&&!data.isIndex}} <div class=menu-extended> <div class=extended-inner> <div class=clearfix> <div class=menu-outer> {{if data.extList&&data.extList.length}}<div class=menu-t>核心模块</div>{{/if}} <ul class=menu> {{each data.coreList as val}} <li class=menu-list> <a href=\"#!/{{data.infos.loader}}/{{data.infos.ver}}/{{val.name}}\">{{val.dName}}</a> </li> {{/each}} </ul> </div> {{if data.extList&&data.extList.length}} <div class=menu-outer> <div class=menu-t>扩展模块</div> <ul class=menu> {{each data.extList as val}} <li class=menu-list> <a href=\"#!/{{data.infos.loader}}/{{data.infos.ver}}/{{val.name}}\">{{val.dName}}</a> </li> {{/each}} </ul> </div> {{/if}} </div> </div> </div> {{/if}}",
         init: function() {
             this.observeLocation({
                 pathname: true,
@@ -49,17 +49,20 @@ KISSY.add('apiapp/mviews/partials/header', function(S, View, Crox, MM, Magix) {
                 q: val
             });
         },
-        'toggleMenu<click>': function() {
+        'toggleMenu<click>': function(e) {
             var menu = S.one('.menu-extended');
             var height = menu.height();
             if (!height) {
+                var h = (e.params.ver == '1.0' || e.params.ver == '1.1') ? 234 : 280;
                 menu.animate({
-                    height: '234px'
+                    height: h
                 }, 0.4, 'easeOut');
+                this.$dropShown = true;
             } else {
                 menu.animate({
                     height: '0'
                 }, 0.4, 'easeBoth');
+                this.$dropShown = false;
             }
         },
         'search<click,keydown>': function(e) {
@@ -69,6 +72,11 @@ KISSY.add('apiapp/mviews/partials/header', function(S, View, Crox, MM, Magix) {
                 }
             } else {
                 this.search();
+            }
+        },
+        '$root<click>': function(e) {
+            if (this.$dropShown && !this.inside(e.target)) {
+                this['toggleMenu<click>']();
             }
         }
     });
