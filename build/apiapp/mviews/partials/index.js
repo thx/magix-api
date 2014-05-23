bbbd195e0ffbbb9ccc435e1e05a4fb636f13f8a9
xinglie.lkf <xinglie.lkf@taobao.com>
2014-05-23 17:25:47 +0800

    修正未按版本区分结果的bug

--- build/apiapp/mviews/partials/index.js
+++ build/apiapp/mviews/partials/index.js
@@ -5,33 +5,35 @@ KISSY.add('apiapp/mviews/partials/index', function(S, View, MM, Crox, Magix) {
      return View.extend({template:"<div class=\"clearfix dir\">\r\n    {{if data.extList&&data.extList.length}}<div class=\"menu-t\">核心模块</div>{{/if}}\r\n    <ul class=\"menu\">\r\n        {{infos=data.infos}}\r\n        {{each data.coreList as one}}\r\n        <li class=\"menu-list\">\r\n            <a href=\"#!/{{infos.loader}}/{{infos.ver}}/{{one.name}}\">{{one.dName}}</a>\r\n            <span class=\"desc\">({{data.extInfo(one.name)}})</span>\r\n        </li>\r\n        {{/each}}\r\n    </ul>\r\n    {{if data.extList&&data.extList.length}}\r\n    <div class=\"menu-t\">扩展模块</div>\r\n    <ul class=\"menu\">\r\n        {{each data.extList as one}}\r\n        <li class=\"menu-list\">\r\n            <a href=\"#!/{{infos.loader}}/{{infos.ver}}/{{one.name}}\">{{one.dName}}</a>\r\n            <span>({{data.extInfo(one.name)}})</span>\r\n        </li>\r\n        {{/each}}\r\n    </ul>\r\n    {{/if}}\r\n</div>",
         render: function() {
             var me = this;
-            MM.fetchClassInfos(function(e, infos) {
-                if (e) {
-                    me.setViewHTML(me.id, e.msg);
-                }
-                MM.createMRequest(me).fetchAll({
-                    name: 'Class_List'
-                }, function(e, m) {
-                    if (e) {
-                        me.setViewHTML(me.id, e.msg);
-                    } else {
-                        var html = Crox.render(me.template, {
-                            coreList: m.get('coreList'),
-                            extList: m.get('extList'),
-                            infos: Magix.local('APIPathInfo'),
-                            extInfo: function(name) {
-                                console.log(infos, name, infos.map[name]);
-                                var info = infos.map[name];
-                                if (info) {
-                                    return info.get('desc');
+            var r = MM.fetchClassInfos(me);
+            r.next(function(err, info) {
+                if (err) {
+                    me.setViewHTML(me.id, err.msg);
+                } else {
+                    this.fetchAll({
+                        name: 'Class_List'
+                    }, function(e, m) {
+                        if (e) {
+                            me.setViewHTML(me.id, e.msg);
+                        } else {
+                            var html = Crox.render(me.template, {
+                                coreList: m.get('coreList'),
+                                extList: m.get('extList'),
+                                infos: Magix.local('APIPathInfo'),
+                                extInfo: function(name) {
+                                    console.log(info, name, info.map[name]);
+                                    var i = info.map[name];
+                                    if (i) {
+                                        return i.get('desc');
+                                    }
+                                    return 'unfound';
                                 }
-                                return 'unfound';
-                            }
-                        });
-                        me.setViewHTML(me.id, html);
-                    }
-                });
-            }, me);
+                            });
+                            me.setViewHTML(me.id, html);
+                        }
+                    });
+                }
+            });
         }
     });
 }, {
